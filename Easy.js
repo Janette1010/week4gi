56a0b55c3b7eb13df366f936fb44238cacf193bd
Janette <janettealv6@gmail.com>
2022-04-25 14:48:09 -0400

    pushing

---
+++ Easy.js
@@ -0,0 +1,5 @@
+let student1 = ["Nico", "Man", "17"];
+let student2 = ["Maki", "Zoom", "20"];
+let student3 = ["Rena", "moo", "28"];
+
+console.log(`My name is ${student2[0]} ${student2[1]} and I am ${student2[2]} years old!`);
